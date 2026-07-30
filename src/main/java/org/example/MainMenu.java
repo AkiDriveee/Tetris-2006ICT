a61d191163acf0295a48f25ae11e9371a4997081
Aksa Saripalli <saripallihomemail@gmail.com>
2026-07-30 19:53:56 +1000

    Add background image and update button colors on main menu

--- src/main/java/org/example/MainMenu.java
+++ src/main/java/org/example/MainMenu.java
@@ -2,11 +2,17 @@ package org.example;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;     // clickable button UI element
-import javafx.scene.layout.VBox;        // stacks items vertically, one under another
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 
 
 public class MainMenu {
@@ -31,7 +37,7 @@ public class MainMenu {
 
         //main menu title
         Label titleLabel = new Label("TETRIS P39 - MAIN MENU");
-        titleLabel.setStyle("-fx-font-size: 28px; -fx-font-weight: bold;");
+        titleLabel.setStyle("-fx-font-size: 28px; -fx-font-weight: bold; -fx-text-fill: #f3f0f0; -fx-background-color: #000000;");
         VBox.setMargin(titleLabel, new Insets(0, 0, 20, 0));
 
         // vertical box sets 20 pixel gap between buttons and centers everything
@@ -39,16 +45,22 @@ public class MainMenu {
         root.setAlignment(Pos.CENTER);
         root.setStyle("-fx-padding: 40");
 
+        //background
+        Image bgImage = new Image(MainMenu.class.getResourceAsStream("/bg.jpg"));
+        BackgroundSize bgSize = new BackgroundSize(100, 100, true, true, false, true);
+        BackgroundImage bgImg = new BackgroundImage(bgImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, bgSize);
+        root.setBackground(new Background(bgImg));
+
         //sets width of all buttons to 400
         for (Button b : new Button[]{PlayBtn, HScoreBtn, ConfigurationBtn, ExitBtn}) {
             b.setPrefWidth(200);
         }
 
         //button colors
-        PlayBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #252222; -fx-font-weight: 900; -fx-background-color: #42e3f0;");
-        HScoreBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #252222; -fx-font-weight: 900; -fx-background-color: #8910c1;");
-        ConfigurationBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #252222; -fx-font-weight: 900; -fx-background-color: #ed2c98;");
-        ExitBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #252222; -fx-font-weight: 900; -fx-background-color: #ef615d;");
+        PlayBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #ffffff; -fx-font-weight: 900; -fx-background-color: #3a86ff;");
+        HScoreBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #ffffff; -fx-font-weight: 900; -fx-background-color: #8338ec;");
+        ConfigurationBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #ffffff; -fx-font-weight: 900; -fx-background-color: #c02ec2;");
+        ExitBtn.setStyle("-fx-font-size: 16px; -fx-text-fill: #ffffff; -fx-font-weight: 900; -fx-background-color: #e63946;");
 
         Scene scene = new Scene(root, 800, 600); //fixed window size
 
