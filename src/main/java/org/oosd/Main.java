73b29a46e460521e27ddf798c236bced58ec6299
Taj-786 <tajm49107@gmail.com>
2026-08-07 00:06:01 +1000

    first commit

---
+++ src/main/java/org/oosd/Main.java
@@ -0,0 +1,16 @@
+package org.oosd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        HighScore.show(stage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
